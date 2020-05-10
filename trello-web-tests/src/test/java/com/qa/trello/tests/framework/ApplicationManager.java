0cc345b875ea0445fff7bbdc6807fb453d3b90dc
Julia <juliakliot.jk@gmail.com>
2020-05-10 10:27:21 +0300

    Select Browser

--- trello-web-tests/src/test/java/com/qa/trello/tests/framework/ApplicationManager.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/framework/ApplicationManager.java
@@ -2,6 +2,8 @@ package com.qa.trello.tests.framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -11,10 +13,23 @@ public class ApplicationManager    {
     BoardHelper board;
     TeamHelper team;
     SessionHelper session;
+     String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() throws InterruptedException {
-        wd = new ChromeDriver();
+
+
+        if(browser.equals(BrowserType.CHROME)){
+
+            wd = new ChromeDriver();
+        } if(browser.equals(BrowserType.FIREFOX)){
+            wd= new FirefoxDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
        //wait = new WebDriverWait(wd, 20);
         wd.manage().window().maximize();
--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
@@ -1,12 +1,13 @@
 package com.qa.trello.tests.tests;
 
 import com.qa.trello.tests.framework.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws InterruptedException {
