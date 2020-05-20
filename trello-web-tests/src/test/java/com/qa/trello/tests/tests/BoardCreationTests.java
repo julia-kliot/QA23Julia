2b882bd20f0ee80f3d2bd74c68bc4b60d03cdbe9
Julia <juliakliot.jk@gmail.com>
2020-05-20 17:57:11 +0300

    Data provider+Logger

--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/BoardCreationTests.java
@@ -3,35 +3,94 @@ package com.qa.trello.tests.tests;
 import com.qa.trello.model.Board;
 import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class BoardCreationTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() throws InterruptedException {
         Thread.sleep(3000);
-       if (!app.getBoard().isOnBoardsPage()) {
+        if (!app.getBoard().isOnBoardsPage()) {
             app.getBoard().waitForElementLocatedAndClick(By.cssSelector("[href$=boards]"), 20);
-           // app.getBoard().goToBoardsPageURL("juliakliotjk");
+            // app.getBoard().goToBoardsPageURL("juliakliotjk");
 
-       }
-   }
-    @Test
-    public void testBoardCreation() throws InterruptedException {
+        }
+    }
+
+
+    @DataProvider
+    public Iterator<Object[]> validBoardsEasy() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"aaaaaaa", "blue"});
+        list.add(new Object[]{"1111111", "green"});
+        list.add(new Object[]{"dfghd_tyuui-bnvmcl", "red"});
+
+        return list.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validBoards() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader =
+                new BufferedReader(new FileReader(new File("src/test/resources/boards.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new Board().withName(split[0])});
+            reader.readLine();
+        }
+
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validBoardsEasy")
+    public void testBoardCreation(String boardName, String boardColor) throws InterruptedException {
         Thread.sleep(7000);
-      //int before = app.getBoard().getBoardsCount();
+        Board board= new Board().withName(boardName).withColor(boardColor);
+        //int before = app.getBoard().getBoardsCount();
         app.getBoard().initBoardCreation();
-        app. getBoard().fillBoardForm(
-                new Board()
-                .withName("newBoard_3"+System.currentTimeMillis())
-                .withColor("green"));
-        app. getBoard().confirmBoardCreation();
+        app.getBoard().fillBoardForm(board);
+
+        // .withColor("green"));
+        app.getBoard().confirmBoardCreation();
         Thread.sleep(2000);
-        //app. getBoard().returnToHomePage();
-       // int after = app.getBoard().getBoardsCount();
+        app. getBoard().returnToHomePage();
+        // int after = app.getBoard().getBoardsCount();
         //Assert.assertEquals(after, before + 1);
 
 
     }
+    @Test(dataProvider = "validBoards")
+    public void testBoardCreation(Board board) {
+        //int before = app.getBoard().getBoardsCount();
+        app.getBoard().initBoardCreation();
+        app.getBoard().fillBoardForm(board);
+        app.getBoard().confirmBoardCreation();
+        app.getBoard().returnToHomePage();
+        //int after = app.getBoard().getBoardsCount();
 
+       // Assert.assertEquals(after, before + 1);
+
+    }
+
+    @Test
+    public void testBoardCreationOld() throws InterruptedException {
+
+          // int before = app.getBoard().getBoardsCount();
+            // System.out.println(new File(".").getAbsolutePath());
+            app.getBoard().initBoardCreation();
+            app.getBoard().fillBoardForm(
+                    new Board()
+                            .withName("Test "+ System.currentTimeMillis())
+                            .withColor("red"));
+            app.getBoard().confirmBoardCreation();
+           // app.getBoard().returnToHomePage();
+            //int after = app.getBoard().getBoardsCount();
 
 }
+    }
--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/TestBase.java
@@ -2,28 +2,51 @@ package com.qa.trello.tests.tests;
 
 import com.qa.trello.tests.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeSuite
     public void setUp() throws InterruptedException {
         app.init();
+
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m) {
+
+
+        logger.info("Start test" + m.getName());
+
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m, Object[] p) {
+        logger.info("Stop test" + m.getName()+ "with parameters"+ Arrays.asList(p));
+
     }
 
 
-     @AfterSuite
+    @AfterSuite
     public void tearDown() {
 
         app.stop();
     }
 
-   // public void changeName() throws InterruptedException {
-
-
+    // public void changeName() throws InterruptedException {
 
 
 }
